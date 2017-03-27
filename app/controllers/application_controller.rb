aa78ea3bfe781a37efc40066e9c5fa247ed3133b
michael budde <michael.s.budde@gmail.com>
2017-03-27 11:10:08 -0700

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  @hostname = request.host || "www.gzcom.us"
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
